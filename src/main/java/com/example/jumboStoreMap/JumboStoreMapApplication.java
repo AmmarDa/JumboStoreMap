26083dbfd5ac5eebacab763efd38cdb8e18ce337
ammar.darwesh <ammar.darwesh@jumbo.com>
2022-09-29 13:31:57 +0200

    initital commit 2

---
+++ src/main/java/com/example/jumboStoreMap/JumboStoreMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.jumboStoreMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumboStoreMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumboStoreMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jumboStoreMap/JumboStoreMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jumboStoreMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumboStoreMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
